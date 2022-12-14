b760bbc290c7b0cb5d2d359e0971b0bdead1304f
Aashish-Dahal <ashishdahal490@gmail.com>
2022-12-14 16:17:04 +0545

    feat: app colors

---
+++ lib/app/core/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color purple = Color(0xff3f41ef);
+  static const Color greyLight = Color(0xffddd9ea);
+  static const Color red = Color(0xffd4281d);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+}
