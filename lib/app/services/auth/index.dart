85c81da84c8fb0e9b22490b1f1c39fdff8362fa6
Aashish-Dahal <ashishdahal490@gmail.com>
2022-12-19 17:35:55 +0545

    fix: added auth state change notifier

--- lib/app/services/auth/index.dart
+++ lib/app/services/auth/index.dart
@@ -1,13 +1,18 @@
 import 'package:dartz/dartz.dart' show Either, left, right;
 import 'package:firebase_auth/firebase_auth.dart' show FirebaseException, User;
-import 'package:flutter_riverpod/flutter_riverpod.dart' show Provider, Ref;
+import 'package:flutter_riverpod/flutter_riverpod.dart'
+    show Provider, Ref, StreamProvider;
 import '../../providers/general/firebase_provider.dart' show firebaseProvider;
 
 final authServiceProvider = Provider<AuthService>((ref) => AuthService(ref));
+final authStateServiceProvider = StreamProvider<User?>(
+    (ref) => ref.watch(authServiceProvider).authStateChange);
 
 class AuthService {
   final Ref _ref;
   AuthService(this._ref);
+  Stream<User?> get authStateChange =>
+      _ref.read(firebaseProvider).authStateChanges();
   Future<Either<String, User?>> firebaseLogin(
       Map<String, dynamic>? data) async {
     try {
@@ -17,7 +22,13 @@ class AuthService {
               email: data!['email'], password: data['password']);
       return right(response.user!);
     } on FirebaseException catch (e) {
-      return left(e.message ?? 'Login failed');
+      if (e.code == 'wrong-password') {
+        return left('Wrong Password');
+      } else if (e.code == 'user-not-found') {
+        return left('User not found');
+      } else {
+        return left('Login failed');
+      }
     }
   }
 
@@ -30,7 +41,12 @@ class AuthService {
               email: data!['email'], password: data['password']);
       return right(response.user!);
     } on FirebaseException catch (e) {
-      return left(e.message ?? 'Login failed');
+      print(e.code);
+      return left(e.message ?? 'Register failed');
     }
+  }
+
+  Future<void> logout() async {
+    await _ref.read(firebaseProvider).signOut();
   }
 }
