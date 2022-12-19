0056e61ec759d8fb82e124641bf9a08679df6987
Aashish-Dahal <ashishdahal490@gmail.com>
2022-12-19 17:34:30 +0545

    feat: logout functionality

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_skeleton/app/services/auth/index.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Page'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text('Home Page'),
+        const SizedBox(
+          height: 10,
+        ),
+        Consumer(
+          builder: (context, ref, child) {
+            return ElevatedButton(
+                onPressed: () {
+                  ref.watch(authServiceProvider).logout();
+                },
+                child: const Text('Logout'));
+          },
+        )
+      ],
     );
   }
 }
