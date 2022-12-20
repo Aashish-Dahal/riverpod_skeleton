f0b66a04f16256f6246429c1e0fed37cc6c9cc3a
Aashish-Dahal <ashishdahal490@gmail.com>
2022-12-20 18:01:33 +0545

    feat: setup translation

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/localization/l10n.dart';
 import 'package:riverpod_skeleton/app/config/themes/app_theme.dart';
 import 'package:riverpod_skeleton/app/providers/router/router_provider.dart';
 
@@ -14,9 +17,16 @@ class App extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
     return MaterialApp.router(
+      debugShowCheckedModeBanner: kDebugMode,
       scaffoldMessengerKey: messengerKey,
       theme: AppTheme.light,
+      locale: context.locale,
       title: 'Riverpod Skeleton',
+      localizationsDelegates: [
+        FormBuilderLocalizations.delegate,
+        ...context.localizationDelegates
+      ],
+      supportedLocales: context.supportedLocales,
       routerConfig: router,
     );
   }
